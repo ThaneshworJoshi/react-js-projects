8482b5209201cfa802810fc5320c01b3d8095259
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-13 11:45:19 +0545

    add: handleChange function to update todoText state variable

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const TodoForm = () => {
+const TodoForm = (props) => {
+  // {todoText, handleTodoTextChange}
   return (
     <form onSubmit={() => {}}>
       <label htmlFor='new-todo'>What needs to be done?</label>
-      <input type='text' id='new-todo' onChange={() => {}} value='' />
+      <input
+        type='text'
+        id='new-todo'
+        onChange={(event) => props.handleTodoTextChange(event)}
+        value={props.todoText}
+      />
       <button>Add Todo</button>
     </form>
   );
--- src/containers/TodoApp.jsx
+++ src/containers/TodoApp.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+import TodoForm from '../components/TodoForm';
+import TodoList from '../components/TodoList';
 
 const TodoApp = () => {
   const [items, setItems] = useState([]);
   const [todoText, setTodoText] = useState('');
 
+  function handleChange(event) {
+    let newTodoText = event.target.value;
+    setTodoText(newTodoText);
+  }
+
   return (
     <div>
       <h3>REACT TODO APP</h3>
+      <TodoForm todoText={todoText} handleTodoTextChange={handleChange} />
     </div>
   );
 };
