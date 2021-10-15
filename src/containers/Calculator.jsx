cd06438b85fe479861d9e0b402b0836a99f423b8
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-15 08:16:45 +0545

    add: state variable and clickhandler

--- src/containers/Calculator.jsx
+++ src/containers/Calculator.jsx
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import Display from '../components/Display';
 import Keypad from '../components/Keypad';
 import './calculator.css';
 
-const Calculator = ({ data }) => {
+const Calculator = () => {
+  const [data, setData] = useState('');
+
+  const calculate = () => {
+    try {
+      const result = eval(data);
+      setData(result);
+    } catch (e) {
+      setData('error');
+    }
+  };
+
+  const handleClick = (e) => {
+    const value = e.target.getAttribute('data-value');
+    switch (value) {
+      case 'clear':
+        setData('');
+        break;
+      case 'equal':
+        calculate();
+        break;
+      default:
+        setData(data + value);
+    }
+  };
+
   return (
     <div className='calculator'>
       <Display data={data} />
       <Keypad>
-        <Button label='C' value='clear' />
-        <Button label='7' value='7' />
-        <Button label='4' value='4' />
-        <Button label='1' value='1' />
-        <Button label='0' value='0' />
+        <Button onClick={handleClick} label='C' value='clear' />
+        <Button onClick={handleClick} label='7' value='7' />
+        <Button onClick={handleClick} label='4' value='4' />
+        <Button onClick={handleClick} label='1' value='1' />
+        <Button onClick={handleClick} label='0' value='0' />
 
-        <Button label='/' value='/' />
-        <Button label='8' value='8' />
-        <Button label='5' value='5' />
-        <Button label='2' value='2' />
-        <Button label='.' value='.' />
+        <Button onClick={handleClick} label='/' value='/' />
+        <Button onClick={handleClick} label='8' value='8' />
+        <Button onClick={handleClick} label='5' value='5' />
+        <Button onClick={handleClick} label='2' value='2' />
+        <Button onClick={handleClick} label='.' value='.' />
 
-        <Button label='x' value='*' />
-        <Button label='9' value='9' />
-        <Button label='6' value='6' />
-        <Button label='3' value='3' />
-        <Button label='' value='null' />
+        <Button onClick={handleClick} label='x' value='*' />
+        <Button onClick={handleClick} label='9' value='9' />
+        <Button onClick={handleClick} label='6' value='6' />
+        <Button onClick={handleClick} label='3' value='3' />
+        <Button onClick={handleClick} label='' value='null' />
 
-        <Button label='-' value='-' />
-        <Button label='+' size='2' value='+' />
-        <Button label='=' size='2' value='equal' />
+        <Button onClick={handleClick} label='-' value='-' />
+        <Button onClick={handleClick} label='+' size='2' value='+' />
+        <Button onClick={handleClick} label='=' size='2' value='equal' />
       </Keypad>
     </div>
   );
