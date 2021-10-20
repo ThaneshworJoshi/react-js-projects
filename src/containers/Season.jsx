745267ae1b54f2aa295858dd44296dd1444dbf54
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-20 08:05:02 +0545

    add: get user current latitude

--- src/containers/Season.jsx
+++ src/containers/Season.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Season = () => {
   const [lat, setlatitude] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
 
-  return <div></div>;
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => setlatitude(position.coords.latitude),
+      (error) => setErrorMessage(error.message)
+    );
+  }, []);
+
+  const renderContent = () => {
+    if (errorMessage && !lat) return <div>Error: {errorMessage}</div>;
+  };
+
+  return <div>{renderContent()}</div>;
 };
 
 export default Season;
