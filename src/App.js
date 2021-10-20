a033d59d89b30e859f188b891b143539575c1b5e
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-20 13:18:04 +0545

    add: css rules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Season from './containers/Season';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Season />
+    </div>
+  );
 }
 
 export default App;
