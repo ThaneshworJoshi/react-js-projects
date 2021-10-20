123a84ba074cbd6d25421fd7e01d39cc1c6cfca6
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-20 07:08:21 +0545

    add: state variables

--- src/containers/Season.jsx
+++ src/containers/Season.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Season = () => {
+  const [lat, setlatitude] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
   return <div></div>;
 };
 
