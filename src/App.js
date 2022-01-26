220ec2657bc29d1469441d339238fc9e12487946
thaneshworjoshi <thaneshwor10@gmail.com>
2022-01-26 21:28:47 +0545

    todo day1 task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return <div className='App'></div>;
+    this.state = {
+      todoList: ['Learn React', 'Learn Python'],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo List App In React</h1>
+        <div>
+          <input type='text' value='Learn React' />
+          <button>Add Todo</button>
+
+          <div>
+            <ul>
+              {this.state.todoList.map((todoItem) => {
+                return <li>{todoItem}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
