ccc720b9a90511885eb72665de611d9939f15051
thaneshworjoshi <thaneshwor10@gmail.com>
2022-02-07 21:33:46 +0545

    simple todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       todoList: ['Learn React', 'Learn Python'],
-      todoText: 'Cook Food',
+      todoText: '',
     };
   }
 
-  handleClick() {
-    console.log('button clicked');
-  }
+  handleClick = () => {
+    this.setState({ todoList: [this.state.todoText, ...this.state.todoList] });
+    this.setState({ todoText: '' });
+  };
+
+  handleTextChange = (e) => {
+    this.setState({ todoText: e.target.value });
+  };
 
   render() {
     return (
       <div>
         <h1>Todo List App In React</h1>
         <div>
-          <input type='text' value={this.state.todoText} />
+          <input
+            type='text'
+            value={this.state.todoText}
+            onChange={this.handleTextChange}
+          />
 
           <button onClick={this.handleClick}>Add Todo</button>
 
